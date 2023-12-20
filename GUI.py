a3f61d051813d33e969935b350b4b1638f1e4b54
jzhao <yingmu1983@gmail.com>
2023-12-20 08:33:35 +0800

    update

---
+++ GUI.py
@@ -0,0 +1,21 @@
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow
+
+import sys
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+        button = QPushButton("Press Me!")
+
+        self.setFixedSize(QSize(400, 300))
+        self.setCentralWidget(button)
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # WordTest
 Education purpose: extract English words from textbooks and create tests. Track correct and wrong answers and customize tests based on it.
+
+#TODO
+1. Have a DB column for next-date so that:
+- search for today or a specific date in DB is much easier.
+- we can draw a histogram on the next days/weeks to see how many items to learn on that day/week.
